c70562d4382a72ed1d266c462b26376badaf4e07
Leonel Marquez <leomarqz2020@gmail.com>
2021-03-24 22:07:57 -0600

    aprendiendo el funcionamiento de las tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,7 +1,16 @@
-''' Las tuplas a dieferencia de las listas no pueden modificarse o agregar elemento de usando metodos como append '''
+''' 
+Las tuplas a dieferencia de las listas no pueden modificarse o agregar elemento de usando metodos como append,
+las tuplas son listas inmutables osea que no pueden modificarse de la misma manera que las listas normales 
+'''
 mi_tupla = (1,2,3,4,5)
 
 # mi_tupla.append(8) --> si esto se ejecuta lanzara un error.
 # print(mi_tupla)
 
-print(mi_tupla.index(5,1,3))
+rs = mi_tupla.index(2)
+print('indice correspondiente al numero 2 de mi tupla: ',rs)
+
+print('cantidad de elemento de mi tupla: ',len(mi_tupla))
+
+del mi_tupla
+# print(mi_tupla) esto daria error ya que mi tupla fue eliminada en la linea anterior.
